be7078519d42c51193f6453c6939e28ad9aeb949
kwhhhh <64717284+kwhhhh@users.noreply.github.com>
2026-01-22 23:22:01 +0800

    docs: replicate a simple attnetion blocks

--- ST.py
+++ ST.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 class RMSNorm(nn.Module):
 
@@ -18,45 +19,63 @@ class RMSNorm(nn.Module):
 
 class MHAttention(nn.Module):
 
-    def __init__(self, heads, d_model):
-        super(MHAttention).__init__()
+    def __init__(self, heads, d_model, dropout):
+        super().__init__()
         assert d_model % heads == 0
         self.d_model = d_model
         self.heads = heads
         self.d_k = d_model // heads
+        # self.dropout = dropout
+        # self.resid_dropout = nn.Dropout(self.dropout)
         self.WQ = nn.Linear(d_model, d_model)
         self.WK = nn.Linear(d_model, d_model)
         self.WV = nn.Linear(d_model, d_model)
         self.Wo = nn.Linear(d_model, d_model)
     
-    def scale_dot_product_attention(self, Q, K, V, mask = None):
-        attn_score = torch.matmul(Q, K.transpose(-2,-1)) / torch.sqrt(self.d_k)
+    # def scale_dot_product_attention(self, Q, K, V, mask = None):
+    #     attn_score = torch.matmul(Q, K.transpose(-2,-1)) / torch.sqrt(self.d_k)
 
-        if mask is None:
-            attn_score = attn_score
-        else:
-            attn_score = attn_score.masked_fill(mask=0, -1e9)
+    #     if mask is None:
+    #         attn_score = attn_score
+    #     else:
+    #         attn_score = attn_score.masked_fill(mask==0, -1e9)
 
-        attn_probs = torch.softmax(attn_score, dim = -1)
-        score = torch.matmul(attn_probs, V)
+    #     attn_probs = torch.softmax(attn_score, dim = -1)
+    #     score = torch.matmul(attn_probs, V)
 
-        return score
+    #     return score
 
     def forward(self, x):
+        bsz, seq_len, _ = x.shape
+        xk, xq, xv = self.WK(x), self.WQ(x), self.WV(x)
+        xk = xk.view(bsz, seq_len, self.heads, self.d_k).transpose(1, 2)
+        xq = xq.view(bsz, seq_len, self.heads, self.d_k).transpose(1, 2)
+        xv = xv.view(bsz, seq_len, self.heads, self.d_k).transpose(1, 2)
 
+        output = F.scaled_dot_product_attention(xk, xq, xv, dropout_p=0.01)
+        attn_out0 = output.transpose(1, 2).contiguous().view(bsz, seq_len, -1)  # (bsz, seq_len, d_model)
 
+        # output.transpose(1,2).reshape(bsz, seq_len, -1)
+        # output = self.Wo(self.resid_dropout(output))
+        output0 = self.Wo(attn_out0)
+        return output
 
 class TransBlock():
 
-    def __init__(self, d_model, heads, inter_d):
-        self.Attention = Attention(heads, d_model)
+    def __init__(self, d_model, heads, inter_d, dropout=0.01):
+        self.Attention = MHAttention(heads, d_model)
         self.FNN = nn.Sequential(nn.Linear(d_model, inter_d),
                                  nn.ReLU(),
+                                 nn.Dropout(dropout),
                                  nn.Linear(inter_d, d_model))
+        self.norm1 = RMSNorm(d_model)
+        self.norm2 = RMSNorm(d_model)
         
     def forward(self, x):
         x = self.Attention(x)
         x = RMSNorm(x) + x
+
+        pass
 
 class Encoder():
 
@@ -72,3 +91,12 @@ class Encoder():
     def forward(self, x):
         x = self.PosEmbedding(x)
         x = self.Transformer(x)
+
+        pass
+
+if __name__ == "__main__":
+    Attn = MHAttention(5, 100, 0.01)
+    x = torch.arange(1200, dtype=torch.float32).reshape(3,4,100)
+    print(x)
+
+    print(Attn(x))
