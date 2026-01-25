6b25932448e32987f2f0a3adfd0cd46dd49fa722
kwhhhh <64717284+kwhhhh@users.noreply.github.com>
2026-01-25 22:27:52 +0800

    docs: more codes in simple transformer

--- ST.py
+++ ST.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class RMSNorm(nn.Module):
 
     def __init__(self, dim, eps = 1e-6):
-        super.__init__()
+        super().__init__()
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(dim))
     
@@ -53,38 +53,51 @@ class MHAttention(nn.Module):
         xv = xv.view(bsz, seq_len, self.heads, self.d_k).transpose(1, 2)
 
         output = F.scaled_dot_product_attention(xk, xq, xv, dropout_p=0.01)
-        attn_out0 = output.transpose(1, 2).contiguous().view(bsz, seq_len, -1)  # (bsz, seq_len, d_model)
+        attn_out = output.transpose(1, 2).contiguous().view(bsz, seq_len, -1)  # (bsz, seq_len, d_model)
 
         # output.transpose(1,2).reshape(bsz, seq_len, -1)
         # output = self.Wo(self.resid_dropout(output))
-        output0 = self.Wo(attn_out0)
+        output = self.Wo(attn_out)
         return output
 
-class TransBlock():
+class TransBlock(nn.Module):
 
     def __init__(self, d_model, heads, inter_d, dropout=0.01):
-        self.Attention = MHAttention(heads, d_model)
+        super().__init__()
+        self.Attention = MHAttention(heads, d_model,dropout)
         self.FNN = nn.Sequential(nn.Linear(d_model, inter_d),
                                  nn.ReLU(),
                                  nn.Dropout(dropout),
                                  nn.Linear(inter_d, d_model))
         self.norm1 = RMSNorm(d_model)
         self.norm2 = RMSNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
-        x = self.Attention(x)
-        x = RMSNorm(x) + x
+        attn_out = self.Attention(x)
+        x = self.norm1(x + self.dropout(attn_out))
+        FFN_output = self.FNN(x)
+        x = self.norm2(x + self.dropout(FFN_output))
 
-        pass
+        return x
 
-class Encoder():
+        # pass
+
+class Encoder(nn.Module):
 
     def __init__(self, d_model, intput_dim, output_dim, vocab_size, N, heads, inter_d, dropout):
+        super().__init__()
         self.PosEmbedding = nn.Linear(vocab_size, d_model)
-        self.Transformer = nn.Sequential()
-        for _ in range(N):
-            self.Transformer.add_module(TransBlock(d_model, heads, inter_d))
-        self.dropout = dropout
+        # self.Transformer = nn.Sequential()
+        self.Transformer = nn.Sequential(
+            TransBlock(d_model, heads, inter_d),
+            TransBlock(d_model, heads, inter_d),
+            TransBlock(d_model, heads, inter_d)
+        )
+        # nn.Sequential()
+        # for _ in range(N):
+        #     self.Transformer.add_module(TransBlock(d_model, heads, inter_d))
+        self.dropout = nn.Dropout(dropout)
         self.output = nn.Linear(d_model, vocab_size)
 
     
@@ -92,11 +105,12 @@ class Encoder():
         x = self.PosEmbedding(x)
         x = self.Transformer(x)
 
-        pass
+        return self.output(self.dropout(x))
 
 if __name__ == "__main__":
-    Attn = MHAttention(5, 100, 0.01)
+    Attn = Encoder(1000000, 30, 100, 100, 3, 5, 16, 0.01)
     x = torch.arange(1200, dtype=torch.float32).reshape(3,4,100)
     print(x)
-
-    print(Attn(x))
+    output = Attn(x)
+    print(output)
+    print(output.shape)
